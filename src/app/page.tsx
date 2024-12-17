88cadca180716d544172ceaf5fc4aec514c50e2e
Anuj Sharma <anuj.sharma@travclan.com>
2024-12-17 15:12:04 +0530

    Add: hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,9 @@
-const styles = {
-  paddingX: "sm:px-16 px-6",
-  paddingY: "sm:py-16 py-6",
-  padding: "sm:px-16 px-6 sm:py-16 py-10",
-
-  heroHeadText:
-    "font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
-  heroSubText:
-    "text-[#333333] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
-
-  sectionHeadText:
-    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
-  sectionSubText:
-    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
-};
+import HeroSection from "../components/HeroSection";
 
 export default function Page() {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
-      <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-      >
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
-        </div>
-
-        <div>
-          <h1 className={`${styles.heroHeadText}`}>
-            Hi, I&#39;m <span className="text-[#333333]">Anuj</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white`}>
-            I am a frontend developer specializing in building interactive and
-            user-friendly web applications.
-          </p>
-        </div>
-      </div>
+      <HeroSection />
     </section>
   );
 }
