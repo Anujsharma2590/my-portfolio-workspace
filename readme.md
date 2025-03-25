d79aee6c60c92b12d8f54fba74b0e4e4b668601d
Anuj Sharma <anuj.sharma@travclan.com>
2025-03-25 11:19:57 +0530

    Remove: tailwind CSS

--- readme.md
+++ readme.md
@@ -2,10 +2,6 @@ https://realfavicongenerator.net/
 
 Font optimisation in next js(when we use font using script it do layout shift(Bad UX))
 
-
-
-
-
 https://claritydev.net/blog
 
 https://www.kirandash.com/
@@ -19,3 +15,5 @@ https://minimal-nextjs-portfolio-website.vercel.app/
 https://dev.to/themeselection/10-trending-animated-ui-component-libraries-4joe?ref=dailydev
 
 https://medium.com/@mdtaqui.jhar/6-repos-to-master-nextjs-d01672dbea7c
+
+![Flow Diagram](/flow.png "Workflow Overview")
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,10 @@ import { Metadata } from "next";
 import React, { ReactNode } from "react";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Footer from "../components/Footer";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { siteMetadata } from "../utils/siteMetaData";
-import Navbar from "../components/Navbar";
-import { ThemeProvider } from "../components/theme-provider";
+
 
 const inter = Inter({
   subsets: ["latin"],
@@ -62,22 +60,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className={`${inter.variable} ${roboto_mono.variable}`}>
-      <body className="min-h-screen grainy">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navbar />
-          <main role="main">
-            {children}
-            <Analytics />
-            <SpeedInsights />
-          </main>
-          {/* Footer */}
-          <Footer />
-        </ThemeProvider>
+      <body>
+
       </body>
     </html>
   );
